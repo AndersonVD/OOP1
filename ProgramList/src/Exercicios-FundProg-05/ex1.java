9a934417a16e23d5885b4fe59e41634372106c33
Anderson Viana <anderson@Offertech.com.br>
2022-08-19 11:51:17 -0300

    Try fix debug console

--- ProgramList/src/Exercicios-FundProg-05/ex1.java
+++ ProgramList/src/Exercicios-FundProg-05/ex1.java
@@ -1,3 +1,15 @@
-public static ex1 name(String[] args) {
-    public static 
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o valor do produto: ");
+        double valor = input.nextDouble();
+        System.out.println("Digite o valor do desconto: ");
+        double desconto = input.nextDouble();
+        System.out.println("Digite o valor do acrescimo: ");
+        double acrescimo = input.nextDouble();
+        double valorFinal = valor - (valor * (desconto / 100)) + (valor * (acrescimo / 100));
+        System.out.println("O valor final é: " + valorFinal);
+    }
 }
